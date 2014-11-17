9dc026850ea82e518f09db795bd16c962df7074e
fraca <marcofraca@gmail.com>
2014-11-17 16:07:38 +0100

    comm

--- PWGS_filtmerge.sh
+++ PWGS_filtmerge.sh
@@ -42,15 +42,13 @@ fi
 
 while [ $i -lt $len ]; do
   echo "$i: ${array[$i]}" >> $nome2"_cov"
-  head -7 ${array[$i]}_mystat >> $nome2"_cov"
+  tail -8 ${array[$i]}_mystat >> $nome2"_cov"
   rm ${array[$i]}_mystat
   echo -e "#######\n\n" >> $nome2"_cov"
   rm ${array[$i]}".bam" 
   rm ${array[$i]}".bam.bai"
 let i++
 done
-
-
 
 
 echo "########################################" >> $nome2"_cov"
@@ -71,16 +69,6 @@ echo 'reads after picard MarkDuplicates' >> $nome2"_cov"
 $bin_dir"samtools" view  -c $nome"_rd.bam" >> $nome2"_cov"
 
 #####################################################################
-###Filtering mapped reads
-#####################################################################
-
-$bin_dir"samtools" view -@ $n_threads -q $alg_qual -f 0x0002 -F 0x0004 -F 0x0008 -b $nome"_rd.bam" > $nome"_filt.bam"
-
-echo 'reads filtered' >> $nome2"_cov"
-
-$bin_dir"samtools" view -c $nome"_filt.bam" >> $nome2"_cov"
-
-#####################################################################
 ##dividing bam file based on bed files
 #####################################################################
 
@@ -92,11 +80,21 @@ un_nome_bed=(`echo ${un_nome_bed//-/ } ` )
 j=0
 lon=${#un_array_bed[*]}
 while [ $j -lt $lon ]; do
-  $bin_dir"samtools" view -@ $n_threads -L ${un_array_bed[$j]} $nome"_filt.bam" -b > $nome2"_"${un_nome_bed[$j]}.bam
+  $bin_dir"samtools" view -@ $n_threads -L ${un_array_bed[$j]} $nome"_rd.bam" -b > $nome2"_"${un_nome_bed[$j]}.bam
   $bin_dir"samtools" index $nome2"_"${un_nome_bed[$j]}.bam
   
   $bin_dir"/bedtools/genomeCoverageBed" -ibam $nome2"_"${un_nome_bed[$j]}.bam > $nome2"_"${un_nome_bed[$j]}"_bedtools"  
   grep "^genome" $nome2"_"${un_nome_bed[$j]}"_bedtools" |  awk ' {if($2>='$min' && $2<='$max') {{bla+=$5; mpond+=$2*$3; sum+=$3}}} END { print "'${un_nome_bed[$j]}': (cov min-max '$min'-'$max') genome covered",bla,"% mean read depth",mpond/sum}' >> $nome2"_cov"
+  
+  i=0
+  cut -f1 ${un_array_bed[$j]} | sed 1d > $nome.nonso
+  IFS=$'\r\n' GLOBIGNORE='*' :; scafs=($(cat $nome.nonso))
+  
+  len=${#scafs[*]}
+  while [ $i -lt $len ]; do
+    grep -P "^"${scafs[$i]}"\t" $nome2"_"${un_nome_bed[$j]}"_bedtools" |  awk ' {if($2>='$min' && $2<='$max') {{bla+=$5; mpond+=$2*$3; sum+=$3}}} END { print " '${scafs[$i]}': (cov min-max '$min'-'$max') genome covered",bla,"% mean read depth",mpond/sum}' >> $nome2"_cov"  
+  let i++
+  done
   
 let j++
 done
--- PWGS_paired.sh
+++ PWGS_paired.sh
@@ -22,21 +22,31 @@ nome="bla_paired_$nome2"
 perl $bin_dir"/basic-pipeline/trim-fastq.pl" --input1 $lane1 --input2 $lane2 --output $nome --quality-threshold $min_qual --min-length 50 --no-5p-trim --disable-zipped-output --fastq-type sanger | grep -A 4 FINISHED > $nome2"_mystat"
 
 
-##alignement bwa  0.7.5a-r405 (version on galileo/kepler you have to use it)
+##alignement bwa mem
 ##remember to index the reference genome.
 
 $bin_dir"bwa" mem -M -t $n_threads $path_gen".fasta" $nome"_1" $nome"_2" > $nome".sam" #let -M maybe error on Picard MarkDuplicates
 
-##samtools 0.1.19-44428cd (same version on server and office PC)
+
 
 $bin_dir"samtools" view -@ $n_threads -Sb $nome".sam" > $nome".bam"
 
-$bin_dir"samtools" sort -@ $n_threads $nome".bam" $nome2
+echo 'All reads in bam' >> $nome2"_mystat"
+$bin_dir"samtools" view -c $nome".bam" >> $nome2"_mystat" #error core fault if I put -c and -2 together
+
+#####################################################################
+###Filtering mapped reads
+#####################################################################
+
+$bin_dir"samtools" view -@ $n_threads -q $alg_qual -f 0x0002 -F 0x0004 -F 0x0008 -b $nome".bam" > $nome"_filt.bam"
+
+$bin_dir"samtools" sort -@ $n_threads $nome"_filt.bam" $nome2
 
 $bin_dir"samtools" index $nome2".bam"
 
-echo 'All reads in bam' >> $nome2"_mystat"
-$bin_dir"samtools" view -c $nome2".bam" >> $nome2"_mystat" #error core fault if I put -c and -2 together
+echo 'reads filtered' >> $nome2"_mystat"
+
+$bin_dir"samtools" view -c $nome2".bam" >> $nome2"_mystat"
 
 
 rm "$nome"*
